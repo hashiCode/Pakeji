94c1fcb690b34cde4fbfc561f59b19d59c25404f
Scott Takahashi <takahashi.scott@gmail.com>
2021-04-25 18:23:49 -0300

    init PackagesView
    Changing in App file

---
+++ Pakeji/Source/Packages/View/PackagesView.swift
@@ -0,0 +1,20 @@
+//
+//  PackagesView.swift
+//  Pakeji
+//
+//  Created by Scott Takahashi on 25/04/21.
+//
+
+import SwiftUI
+
+struct PackagesView: View {
+    var body: some View {
+        Text("Packages View")
+    }
+}
+
+struct PackagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        PackagesView()
+    }
+}
--- Pakeji/Support Files/PakejiApp.swift
+++ Pakeji/Support Files/PakejiApp.swift
@@ -13,7 +13,7 @@ struct PakejiApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            PackagesView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
