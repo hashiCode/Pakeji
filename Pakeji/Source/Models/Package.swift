b650a693f414d9daba5d5788beb8f81a0ab0c29f
Scott Takahashi <takahashi.scott@gmail.com>
2021-04-27 21:29:16 -0300

    initial definition of package model

---
+++ Pakeji/Source/Models/Package.swift
@@ -0,0 +1,14 @@
+//
+//  Package.swift
+//  Pakeji
+//
+//  Created by Scott Yamagami Takahashi on 27/04/21.
+//
+
+import Foundation
+
+struct Package {
+    var id: UUID
+    var name: String
+    var notes: String
+}
